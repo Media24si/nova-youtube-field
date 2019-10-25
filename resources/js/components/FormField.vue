ca18ac9f39c358f3ec4720abc46f4189c179d12c
Noël Hagestein <contact@noel.codes>
2019-10-25 10:55:56 +0200

    Set input id right so label click works

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,7 +1,7 @@
 <template>
     <default-field :field="field">
         <template slot="field">
-            <input :id="field.name" type="text"
+            <input :id="field.attribute" type="text"
                 class="w-full form-control form-input form-input-bordered"
                 :class="errorClasses"
                 :placeholder="field.name"
